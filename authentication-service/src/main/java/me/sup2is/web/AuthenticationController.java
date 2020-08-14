311ffdf76718a3a765854f580cb4d8b42d40daaa
chs0425 <chs0425@estgames.com>
2020-08-14 13:06:06 +0900

    Authentication Service Default Setting

--- authentication-service/src/main/java/me/sup2is/web/AuthenticationController.java
+++ authentication-service/src/main/java/me/sup2is/web/AuthenticationController.java
@@ -1,17 +1,19 @@
-package me.sup2is;
+package me.sup2is.web;
 
-import me.sup2is.web.JsonResult;
+import lombok.RequiredArgsConstructor;
+import me.sup2is.jwt.JwtTokenUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthenticationController {
 
-
-    @GetMapping("/")
-    public ResponseEntity<JsonResult> generateJwtToken() {
-
+    @GetMapping("/auth")
+    public ResponseEntity<JsonResult> generateJwtToken(@RequestBody MemberDto memberDto) {
+        return null;
     }
 
 }
--- authentication-service/src/main/java/me/sup2is/web/MemberDto.java
+++ authentication-service/src/main/java/me/sup2is/web/MemberDto.java
@@ -1,4 +1,15 @@
 package me.sup2is.web;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@AllArgsConstructor
 public class MemberDto {
+
+    private final String memberId;
+    private final String password;
+
 }

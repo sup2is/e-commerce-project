6c019fd82b9cffe899c28709618eff54a150bbad
chs0425 <chs0425@estgames.com>
2020-08-14 12:59:37 +0900

    Api GW Default Settings

--- api-gateway/src/main/java/me/sup2is/ApiGatewayApplication.java
+++ api-gateway/src/main/java/me/sup2is/ApiGatewayApplication.java
@@ -1,4 +1,14 @@
 package me.sup2is;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
 public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }

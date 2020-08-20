9872899e2e1ff59cca0cdb6c55a1a808099adb93
sup2is <dev.sup2is@gmail.com>
2020-08-20 13:28:15 +0900

    Spring redis session clustering

--- api-gateway/src/main/java/me/sup2is/ApiGatewayApplication.java
+++ api-gateway/src/main/java/me/sup2is/ApiGatewayApplication.java
@@ -3,9 +3,11 @@ package me.sup2is;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableRedisHttpSession
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
--- authentication-service/src/main/java/me/sup2is/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/me/sup2is/AuthenticationServiceApplication.java
@@ -2,8 +2,10 @@ package me.sup2is;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class AuthenticationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationServiceApplication.class, args);

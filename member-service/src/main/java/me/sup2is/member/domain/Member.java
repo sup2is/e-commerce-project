700bfe183628eaa4a2923c9db36c3dd64c19ca74
sup2is <dev.sup2is@gmail.com>
2020-08-25 10:51:36 +0900

    builder 패턴 + 팩토리 메서드 추가

--- member-service/src/main/java/me/sup2is/member/domain/Member.java
+++ member-service/src/main/java/me/sup2is/member/domain/Member.java
@@ -34,4 +34,59 @@ public class Member {
     private List<Authority> authorities = new ArrayList<>();
 
     private boolean enable;
+
+    public static Member createMember(Builder builder) {
+        return builder.build();
+    }
+
+    public static class Builder {
+
+        private String email;
+        private String password;
+        private String name;
+        private String address;
+        private String zipCode;
+        private String phone;
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder zipCode(String zipCode) {
+            this.zipCode = zipCode;
+            return this;
+        }
+
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        private Member build() {
+            Member member = new Member();
+            member.email = this.email;
+            member.password = this.password;
+            member.name = this.name;
+            member.address = this.address;
+            member.zipCode = this.zipCode;
+            member.phone = this.phone;
+            return member;
+        }
+    }
 }

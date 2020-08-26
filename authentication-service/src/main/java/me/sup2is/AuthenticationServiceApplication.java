83beb59c38db3299161a171994fb8afe9c1e572b
sup2is <dev.sup2is@gmail.com>
2020-08-26 10:54:59 +0900

    @EnableFeignClients 추가 및 MemberServiceClient 수정

--- authentication-service/src/main/java/me/sup2is/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/me/sup2is/AuthenticationServiceApplication.java
@@ -2,10 +2,12 @@ package me.sup2is;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableRedisHttpSession
+@EnableFeignClients
 public class AuthenticationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationServiceApplication.class, args);
--- authentication-service/src/main/java/me/sup2is/web/MemberServiceClient.java
+++ authentication-service/src/main/java/me/sup2is/web/MemberServiceClient.java
@@ -1,13 +1,12 @@
 package me.sup2is.web;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "member-service", primary = false)
 public interface MemberServiceClient {
 
     @PostMapping("/member")
-    JsonResult<User> getMember(String email);
+    JsonResult<Member> getMember(String email);
 
 }

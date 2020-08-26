dff8a95bddb567f11cb494f1a00b0e71cde2c643
sup2is <dev.sup2is@gmail.com>
2020-08-26 16:53:21 +0900

    JsonResult에 Form FieldError관련 필드 추가

--- common/src/main/java/me/sup2is/web/ErrorMessage.java
+++ common/src/main/java/me/sup2is/web/ErrorMessage.java
@@ -1,13 +1,15 @@
 package me.sup2is.web;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorMessage {
 
     private String name;
@@ -19,4 +21,12 @@ public class ErrorMessage {
         errorMessage.message = e.getMessage();
         return errorMessage;
     }
+
+    public static List<ErrorMessage> createFieldErrors(List<FieldError> fieldErrors) {
+        return fieldErrors.stream()
+                .map(error -> new ErrorMessage(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+    }
+
+
 }
--- common/src/main/java/me/sup2is/web/JsonResult.java
+++ common/src/main/java/me/sup2is/web/JsonResult.java
@@ -4,43 +4,51 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.validation.FieldError;
 
 import java.util.List;
 
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+//todo 추후에 builder 패턴 고려할것
 public class JsonResult<T> {
 
     private Result result;
     private List<String> messages;
     private ErrorMessage error;
+    private List<ErrorMessage> fieldErrors;
     private T data;
 
     public JsonResult(Result result) {
-        this(result, null, null, null);
+        this(result, null, null, null, null);
     }
 
     public JsonResult(Exception e) {
         this(Result.FAIL
             , null
             , ErrorMessage.createError(e)
+            , null
             , null);
+    }
 
+    public JsonResult(List<FieldError> fieldErrors) {
+        this(Result.FAIL, null, null, ErrorMessage.createFieldErrors(fieldErrors), null);
     }
 
     public JsonResult(T data) {
-        this(Result.SUCCESS, null, null, data);
+        this(Result.SUCCESS, null, null, null, data);
     }
 
-    public JsonResult(Result result, List<String> messages, ErrorMessage error, T data) {
+    public JsonResult(Result result, List<String> messages, ErrorMessage error, List<ErrorMessage> fieldErrors, T data) {
         this.result = result;
         this.messages = messages;
         this.error = error;
+        this.fieldErrors = fieldErrors;
         this.data = data;
     }
 
-    private enum Result {
+    public enum Result {
         SUCCESS, FAIL
     }
 

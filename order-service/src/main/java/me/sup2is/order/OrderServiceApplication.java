0d07847031986f49fde26518c74f8c37f24e7ec6
sup2is <dev.sup2is@gmail.com>
2020-09-01 15:06:24 +0900

    @EnableJpaAuditing 분리

--- order-service/src/main/java/me/sup2is/order/OrderServiceApplication.java
+++ order-service/src/main/java/me/sup2is/order/OrderServiceApplication.java
@@ -3,10 +3,8 @@ package me.sup2is.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableJpaAuditing
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
---
+++ order-service/src/main/java/me/sup2is/order/config/JpaAuditingConfiguration.java
@@ -0,0 +1,8 @@
+package me.sup2is.order.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {}

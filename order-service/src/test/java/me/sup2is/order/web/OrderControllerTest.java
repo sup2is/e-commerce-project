07b2102d6260f4850ae00ff2ffd410da0f58937b
sup2is <dev.sup2is@gmail.com>
2020-09-08 14:02:37 +0900

    spring rest docs 데모 적용 /order(주문) 작성

--- order-service/src/test/java/me/sup2is/order/web/OrderControllerTest.java
+++ order-service/src/test/java/me/sup2is/order/web/OrderControllerTest.java
@@ -1,6 +1,7 @@
 package me.sup2is.order.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.sup2is.order.config.RestDocsConfiguration;
 import me.sup2is.order.domain.OrderItem;
 import me.sup2is.order.domain.dto.OrderItemRequestDto;
 import me.sup2is.order.domain.dto.OrderRequestDto;
@@ -11,9 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -23,10 +28,18 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @WebMvcTest(excludeAutoConfiguration = SecurityAutoConfiguration.class)
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
 class OrderControllerTest {
 
     @Autowired
@@ -37,6 +50,37 @@ class OrderControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("주문 테스트")
+    public void order() throws Exception {
+        //given
+        OrderItemRequestDto orderItemRequestDto1 = new OrderItemRequestDto(1L, 20000L, 4, 0);
+        OrderItemRequestDto orderItemRequestDto2 = new OrderItemRequestDto(1L, 30000L, 2, 0);
+
+        List<OrderItemRequestDto> orderItemRequestDto = Arrays.asList(orderItemRequestDto1, orderItemRequestDto2);
+
+        OrderRequestDto orderRequestDto = new OrderRequestDto(1L, orderItemRequestDto);
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/")
+                .content(objectMapper.writeValueAsString(orderRequestDto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(200))
+                .andDo(document("create-order",
+                        requestFields(
+                                fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 번호"),
+                                fieldWithPath("orderItems").type(JsonFieldType.ARRAY).description("주문 아이템 리스트"),
+                                fieldWithPath("orderItems[].productId").type(JsonFieldType.NUMBER).description("상품 번호"),
+                                fieldWithPath("orderItems[].price").type(JsonFieldType.NUMBER).description("상품 가격"),
+                                fieldWithPath("orderItems[].count").type(JsonFieldType.NUMBER).description("주문 수량"),
+                                fieldWithPath("orderItems[].discountRate").type(JsonFieldType.NUMBER).description("할인율")
+                        )
+                ));
+
+    }
 
     @Test
     @DisplayName("/order 요청시 orderItemRequest 파라미터 검사")
@@ -55,9 +99,7 @@ class OrderControllerTest {
                 .content(objectMapper.writeValueAsString(orderRequestDto))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().is(400))
-                .andReturn();
-
+                .andExpect(status().is(400));
 
     }
 
@@ -77,6 +119,7 @@ class OrderControllerTest {
         //then
 
     }
+
     @Test
     @DisplayName("주문 취소")
     public void order_cancel() throws Exception {
@@ -85,7 +128,6 @@ class OrderControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.post("/1/cancel"))
                 .andDo(print())
                 .andExpect(status().is(200));
-
         //then
 
     }

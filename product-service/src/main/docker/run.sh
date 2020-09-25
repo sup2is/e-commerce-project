4eb92e02511dbd0d69d50c2c9a18097dadd8d7ae
sup2is <dev.sup2is@gmail.com>
2020-09-25 04:19:53 +0000

    docker build script 작성

---
+++ product-service/src/main/docker/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+java $JVM_OPTIONS -Djava.security.egd=file:/dev/./urandom\
+    -Dserver.port=$SERVER_PORT\
+    -Deureka.client.serviceUrl.defaultZone=$EUREKA_SERVER_URL\
+    -Dspring.redis.host=$REDIS_SERVER_URL\
+    -Dspring.redis.port=$REDIS_SERVER_PORT\
+    -Dspring.zipkin.base-url=$ZIPKIN_SERVER_URL\
+    -Dspring.profiles.active=$PROFILE -jar /usr/local/product-service/app.jar

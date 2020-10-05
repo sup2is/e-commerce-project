a6253c09ff5873d8ae869f219e633bff6c9200fd
sup2is <dev.sup2is@gmail.com>
2020-10-05 06:56:10 +0000

    build 스크립트 수정

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-baseDir=${PWD%/*}
+baseDir=/root/e-commerce-project
 
-mvn -f $baseDir/order-service/pom.xml clean package docker:build -DpushImage -Dspring.profiles.active=dev-integration
-mvn -f $baseDir/product-service/pom.xml clean package docker:build -DpushImage -Dspring.profiles.active=dev-integration
-mvn -f $baseDir/authentication-service/pom.xml clean package docker:build -DpushImage -Dspring.profiles.active=dev-integration
-mvn -f $baseDir/member-service/pom.xml clean package docker:build -DpushImage -Dspring.profiles.active=dev-integration
-#mvn -f $baseDir/eureka-server/pom.xml clean package docker:build -DpushImage
-#mvn -f $baseDir/api-gateway/pom.xml clean package docker:build -DpushImage
+mvn -f $baseDir/order-service/pom.xml clean package docker:build -DpushImage
+mvn -f $baseDir/product-service/pom.xml clean package docker:build -DpushImage 
+mvn -f $baseDir/authentication-service/pom.xml clean package docker:build -DpushImage
+mvn -f $baseDir/member-service/pom.xml clean package docker:build -DpushImage
+mvn -f $baseDir/eureka-server/pom.xml clean package docker:build -DpushImage
+mvn -f $baseDir/api-gateway/pom.xml clean package docker:build -DpushImage
